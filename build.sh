431b4855a1d36724c3c2720f174f2c1fe53323b5
Tushar Chugh <techie2sh@gmail.com>
2017-11-17 21:31:24 -0500

    add gcov struct to build

--- build.sh
+++ build.sh
@@ -3,7 +3,12 @@
 echo "Environment: `uname -a`"
 echo "Compiler: `$CXX --version`"
 
-cmake || exit 1
-make || exit 1
 
-
+rm -r build
+mkdir -p build
+cd build
+cmake -DCMAKE_BUILD_TYPE=ON -DBUILD_TESTS=ON .. && make
+make test
+make string
+cd ..
+codecov -t 6cab29fe-1861-468f-903e-61e4fc96107e -X gcov
