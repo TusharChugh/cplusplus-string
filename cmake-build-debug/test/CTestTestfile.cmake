7ada1eaf0e1dd53dec36efb8ae7eca30b76e9a6c
Tushar Chugh <techie2sh@gmail.com>
2017-11-17 21:10:43 -0500

    remove clion stuff

--- cmake-build-debug/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fzff9p/cplusplus_explore/libraries/string/test
-# Build directory: /home/fzff9p/cplusplus_explore/libraries/string/cmake-build-debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
